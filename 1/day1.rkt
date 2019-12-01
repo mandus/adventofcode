2e19d74075c2d0f129bc955814da9f3ee4903e28
Åsmund Ødegård <mandus@gmail.com>
2019-12-01 13:04:47 +0100

    Clean up

--- 1/day1.rkt
+++ 1/day1.rkt
@@ -3,32 +3,24 @@
 
 ;; Advent of Code 2019 - day 1 - Åsmund Ødegård
 
-(define (find-fuels weight fuels)
-  (let ((f (- (floor (/ weight 3)) 2)))
-    (if (> f 0)
-      (find-fuels f (cons f fuels))
-      (for/sum ([i fuels]) i))))
+(define (fuel-for-mass mass)
+  (- (quotient mass 3) 2))
 
-; the extra for/sum can of course be dropped
-(define (find-fuels-II weight fuels)
-  (let ([f (- (floor (/ weight 3)) 2)])
+(define (find-fuels-II mass fuels)
+  (let ([f (fuel-for-mass mass)])
     (if (> f 0)
       (find-fuels-II f (+ f fuels))
-      fuels) ; just return the number
-    ))
+      fuels)))
 
-(define (run-part2)
-  (for/fold
-    ([acc 0])
-    ([w (file->list "input.txt")])
-    (+ acc (find-fuels-II w 0))))
+; part1
+(for/fold
+  ([acc 0])
+  ((i (file->list "input.txt")))
+  (+ acc (fuel-for-mass i)))
 
 
-(define (run-part1)
-    (for/fold
-      ([acc 0])
-      ((i (file->list "input.txt")))
-      (+ acc (- (floor (/ i 3)) 2))))
-
-(run-part1)
-(run-part2)
+; part2
+(for/fold
+  ([acc 0])
+  ([w (file->list "input.txt")])
+  (+ acc (find-fuels-II w 0)))
