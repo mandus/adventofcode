ac002e44f22329abd8e0cb17f8066e6c7b71a460
Åsmund Ødegård <mandus@gmail.com>
2019-12-11 11:14:01 +0100

    update from early day

--- 2/day2_part1.py
+++ 2/day2_part1.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 d = [int(d) for d in open('input.txt').readline().strip().split(',')]
 
 testdata = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
+
 
 def restoredata(data):
     data[1] = 12
@@ -23,23 +24,19 @@ def runit(data):
     while True:
         opcode = data[pos]
         if opcode == 99:
-            print('opcode', opcode)
             break
         left = leftop(data, pos)
         right = rightop(data, pos)
-        print(pos, opcode, left, right)
         if opcode == 1:
             val = left + right
         elif opcode == 2:
             val = left * right
         savepos = data[pos+3]
-        print('save', val, savepos)
         data[savepos] = val
-        print(data)
         pos += 4
     return data
 
+
 d = restoredata(d)
 d = runit(d)
-print d
-print d[0]
+print(d[0])
