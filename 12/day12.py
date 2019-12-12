bf6529bf06dfc3a60232484a80b499d264b30a0c
Åsmund Ødegård <mandus@gmail.com>
2019-12-12 22:32:18 +0100

    still no clue

--- 12/day12.py
+++ 12/day12.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-
+import hashlib
 
 # input:
 moons = [np.array((17, 5, 1)), np.array((-2, -8, 8)), np.array((7, -6, 14)), np.array((1, -10, 4))]
@@ -13,7 +13,22 @@ moons = [np.array((17, 5, 1)), np.array((-2, -8, 8)), np.array((7, -6, 14)), np.
 n = len(moons)
 velo = [np.zeros(3) for i in range(n)]
 
-for k in range(1000):
+
+def tostr(x):
+    return str(x)
+
+
+def md5sum(moons, velo):
+    s = "".join(["|".join(s for s in map(tostr, list(m))) for m in moons]) + \
+        "".join(["|".join(s for s in map(tostr, list(v))) for v in velo])
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+
+hashmap = {}
+
+keeprunning = True
+k = 0
+while keeprunning:
     for i in range(n):
         for j in range(n):
             if i == j:
@@ -22,6 +37,18 @@ for k in range(1000):
             abs_diff = np.array([v or 1. for v in np.abs(diff)])
             velo[i] += diff/abs_diff
     moons = [moons[i]+velo[i] for i in range(n)]
+    hashit = md5sum(moons, velo)
+    if not hashmap.get(hashit, False):
+        # not present, insert
+        hashmap[hashit] = k
+    else:
+        prevk = hashmap[hashit]
+        print("moon/velo (hash {}) also at k = {}; steps between: {}".format(
+            hashit, prevk, k-prevk))
+        keeprunning = False
+    k += 1
+    if not k % 10000:
+        print(k)
 
 # part 1
 print(velo)
