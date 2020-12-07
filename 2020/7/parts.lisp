5277d33de1f0eecaa17f2908109c4a80945e0f6c
Åsmund Ødegård <mandus@gmail.com>
2020-12-07 14:53:04 +0100

    some cleanup

--- 2020/7/parts.lisp
+++ 2020/7/parts.lisp
@@ -28,9 +28,9 @@
 (defun content-list (lst &optional (found nil))
   (let* ((bag (car (cl-ppcre:split " bag" (string-trim " " (car lst)))))
          (first-space (search " " bag))
-         (bag-count (string-trim " " (subseq bag 0 first-space)))
-         (bag-type (string-trim " " (subseq bag first-space)))
-         (nxtfound (cons bag-type (cons bag-count found)))
+         (bag-count (subseq bag 0 first-space))
+         (bag-type (subseq bag  (1+ first-space)))
+         (nxtfound (append (list bag-type bag-count) found))
          (more (cdr lst))
          ) 
     (cond
@@ -53,9 +53,9 @@
          (contained (make-hash-table :test #'equalp))
          )
   (dolist (line data) 
-    (let* ((bag (cl-ppcre:split "contain " line))
-           (name (string-trim " " (car (cl-ppcre:split "bags" (car bag)))))
-           (content (content-list (cl-ppcre:split "," (string-trim "." (cadr bag)))))) 
+    (let* ((bag (cl-ppcre:split " bags contain " line))
+           (name (car bag))
+           (content (content-list (cl-ppcre:split "," (cadr bag))))) 
       (when *debug*
         (format t "~a contains: ~a~%" name content))
       (setf (gethash name bags) content)
@@ -68,8 +68,8 @@
     (dolist (bag candidates)
       (dolist (check (gethash bag contained))
         (when (not (member check found :test #'string=)) 
-          (pushnew check newcandidates :test #'string=))
-        (pushnew check found :test #'string=)
+          (pushnew check newcandidates :test #'string=)
+          (pushnew check found :test #'string=))
         (when *debug*
           (format t "check ~a: cands ~a, found ~a~%" check newcandidates found))))
     (if newcandidates
@@ -121,4 +121,3 @@
 (defun run ()
   (part1 *inp*)
   (part2 *inp*))
-
