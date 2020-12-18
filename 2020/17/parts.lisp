101ca4a846065ed454f9bb5a5acae7bf2373b62e
Åsmund Ødegård <mandus@gmail.com>
2020-12-18 16:05:43 +0100

    generalized day 17 for nD game-of-life

--- 2020/17/parts.lisp
+++ 2020/17/parts.lisp
@@ -3,6 +3,7 @@
 ; run: sbcl --quit --load parts.lisp --eval '(aoc:run)'
 
 (ql:quickload :swank)
+(ql:quickload :alexandria)
 
 (uiop:define-package 
   :aoc
@@ -24,26 +25,28 @@
     (loop for line = (read-line f nil)
           while line collect (funcall trans line))))
 
-(defun parse (lst mem)
+(append (list 1 3) '(0 0))
+
+(defun parse (lst mem &optional (extradim '(0)))
   (loop for line in lst
         for x = 0 then (1+ x)
         do (loop for state in (coerce line 'list)
                  for y = 0 then (1+ y)
-                 for cell = (list x y 0)
+                 for cell = (append (list x y) extradim)
                  when (char= #\# state)
                  do (setf (gethash cell mem) state))))
 
-;; 3d game of life. - need to generalize to N dimensions for part-2.
+;; Nd game of life. 
+
+(defun adj-inner (dim &optional (lst (list '(-1) '(0) '(1)))) 
+  (let ((nxtd (1- dim))
+        (nxtl (mapcan #'(lambda (d) (mapcar #'(lambda (x) (append `(,d) x)) lst)) '(-1 0 1))))
+    (if (< 1 nxtd)
+      (adj-inner nxtd nxtl)
+      (remove-if #'(lambda (l) (zerop (reduce #'+ (mapcar #'abs l)))) nxtl))))
 
 (defun adj (cell)
-  (let ((x (first cell))
-        (y (second cell))
-        (z (third cell)))
-    (loop for i from -1 to 1
-          append (loop for j from -1 to 1
-                       append (loop for k from -1 to 1
-                                    when (or (/= i 0) (/= j 0) (/= k 0))
-                                    collect (list (+ x i) (+ y j) (+ z k)))))))
+  (mapcar #'(lambda (l) (mapcar #'+ cell l)) (adj-inner (length cell))))
 
 (defun active-adj (cell mem)
   (loop for c in (adj cell) 
@@ -57,23 +60,29 @@
       (t (if (= 3 (active-adj cell mem)) #\# #\.)) ; handle either state #\. or nil
     )))
 
+(defun coords (mins maxs &key (extend 0))
+  (labels ((inner (lst &optional (dim 1))
+             (let* ((dmin (nth dim mins))
+                    (dmax (nth dim maxs))
+                    (len (+ 1 (- dmax dmin) (* 2 extend)))
+                    (start (- (nth dim mins) extend))
+                    (nxtlst (mapcan #'(lambda (x) (mapcar #'(lambda (b) (append `(,x) b)) lst)) (alexandria:iota len :start start))))
+               (if (>= (1+ dim) (length mins))
+                   (mapcar #'reverse nxtlst)
+                   (inner nxtlst (1+ dim))))))
+    (let* ((len (+ 1 (- (first maxs) (first mins)) (* 2 extend)))
+           (start (- (first mins) extend)))
+      (inner (mapcar #'list (alexandria:iota len :start start))))))
 
-(defun mem-updates (mem minx maxx miny maxy minz maxz)
-  (loop for i from (1- minx) to (1+ maxx)
-        append (loop for j from (1- miny) to (1+ maxy)
-                 append (loop for k from (1- minz) to (1+ maxz)
-                          for cell = (list i j k)
-                          for state = (gethash cell mem)
-                          for newstate = (update-cell (list i j k) mem)
-                          ;do (when *debug* (format t "m-u: (~a,~a,~a) ~a -> ~a ~%" i j k state newstate))
-                          when (not (equalp state newstate))
-                          collect (cons cell newstate)))))
+(defun mem-updates (mem mins maxs)
+  (loop for cell in (coords mins maxs :extend 1)
+        for state = (gethash cell mem)
+        for newstate = (update-cell cell mem)
+        when (not (equalp state newstate))
+        collect (cons cell newstate)))
 
-
-(defun simulate (mem iter rows cols &optional (layers 1) (minx 0) (miny 0) (minz 0))
-  (let ((updates (mem-updates mem minx rows miny cols minz layers)))
-    ; (when *debug*
-    ;   (format t "updates: ~a~%" updates))
+(defun simulate (mem iter mins maxs)
+  (let ((updates (mem-updates mem mins maxs)))
     (dolist (upd updates)
       (let* ((cell (car upd))
              (state (cdr upd))
@@ -81,47 +90,40 @@
         (when (or curstate (char= state #\#)) ; only update if already in hash or activating
          (setf (gethash cell mem) state))))
 
-    ; (when updates  ; this loops until steady-state, which will never happen..
-    ;   (simulate mem))
-    ;
-    (multiple-value-bind (minx rows miny cols minz layers) (mem-range mem) 
-      (when *debug*
-        (print-mem mem minx rows miny cols minz layers))
+    (multiple-value-bind (mins maxs) (mem-range mem) 
       (when (< 0 iter)
-        (simulate mem (1- iter) rows cols layers minx miny minz)))))
-
-
+        (simulate mem (1- iter) mins maxs)))))
 
 (defun count-active (mem)
   (loop for state being the hash-value of mem
         count (char= state #\#)))
 
 (defun mem-range (mem)
-  (loop for cell being the hash-key of mem
-        for x = (first cell)
-        for y = (second cell)
-        for z = (third cell)
-        for minx = x then (min minx x)
-        for maxx = x then (max maxx x)
-        for miny = y then (min miny y)
-        for maxy = y then (max maxy y)
-        for minz = z then (min minz z)
-        for maxz = z then (max maxz z)
-        finally (return (values minx maxx miny maxy minz maxz))))
+  (let* ((cells (loop for cell being the hash-key of mem collect cell))
+         (mincoord (reduce #'(lambda (x y) (mapcar #'min x y)) cells))
+         (maxcoord (reduce #'(lambda (x y) (mapcar #'max x y)) cells))
+         )
+    (values mincoord maxcoord)))
 
 ;; debug helper
 ;; 
-(defun print-mem (mem minrow rows mincol cols minlayer layers)
-  (loop for k from minlayer to layers
-        do (progn
-             (format t "~% layer ~a ~%~%" k)
-             (loop for i from minrow to rows
+(defun print-mem (mem mins maxs)
+  ; only implemented for 3d
+  (labels ((innerpr (&optional w) 
+             (loop for k from (third mins) to (third maxs)
                    do (progn
-                        (loop for j from mincol to cols
-                              for state = (gethash (list i j k) mem)
-                              do (format t "~a" (or state #\.)))
-                        (format t "~%")))
-             )))
+                        (format t "~% z ~a ~%~%" k)
+                        (loop for i from (first mins) to (first maxs)
+                              do (progn
+                                   (loop for j from (second mins) to (second maxs)
+                                         for cell = (append (list i j k) w)
+                                         for state = (gethash cell mem)
+                                         do (format t "~a" (or state #\.)))
+                                   (format t "~%")))))))
+
+    (when (= (length mins) 3)
+      (innerpr)
+      (format t "~%----- next it -----~%"))))
 
 ;; drivers
 ;;
@@ -133,7 +135,7 @@
 
     (format t "Part 1~%")
     (parse data mem)
-    (simulate mem 5 rows cols)
+    (simulate mem 5 '(0 0 0) `(,rows ,cols 0))
     (format t "after simulation, ~a active ~%" (count-active mem))
 
     (when *debug*
@@ -143,16 +145,17 @@
 
 (defun part2 (fn)
   (let* ((data (read-input fn ))
-         (seats (make-hash-table :test #'equalp)))
+         (rows (length data))
+         (cols (length (car data)))
+         (mem (make-hash-table :test #'equalp)))
     
      (format t "Part 2~%")
-     (parse data seats)
-     (simulate seats 5 #'view-seat)
-     (format t "after simulation, ~a occupied~%" (count-occup seats))
+     (parse data mem '(0 0))
+     (simulate mem 5 '(0 0 0 0) `(,rows ,cols 0 0))
+     (format t "after simulation, ~a active~%" (count-active mem))
 
     (when *debug*
       (format t "data ~a~%" data)
-      (format t "adjacent-view (4,5): ~a ~%" (adjacents (cons 4 5) seats #'view-seat))
       )))
 
 (defun run ()
