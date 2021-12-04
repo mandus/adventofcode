de35d74bb3a7508990bc47da3bf1dbb6b6873707
Åsmund Ødegård <mandus@gmail.com>
2021-12-04 23:44:31 +0100

    day 2, python cleanup

--- 2021/2/parts.py
+++ 2021/2/parts.py
@@ -3,47 +3,32 @@
 part1 = ''
 part2 = ''
 
+fn = 'input.txt'
 
-# data = [
-#     "forward 5",
-#     "down 5",
-#     "forward 8",
-#     "up 3",
-#     "down 8",
-#      "forward 2"]
+data = [line.strip().split() for line in open(fn, 'r')]
 
-data = [l.strip() for l in open('input.txt', 'r').readlines()]
-
-p, d = 0, 0
 ops = {
     'forward': lambda v, p, d: (p+v, d),
     'up': lambda v, p, d: (p, d-v),
     'down': lambda v, p, d: (p, d+v),
     }
 
-for line in data:
-    op, val = line.split()
-    val = int(val)
-    #print(f'{op} > {val} >', ops[op](val, p, d))
-    p, d = ops[op](val, p, d)
+p, d = 0, 0
+for op, val in data: p, d = ops[op](int(val), p, d)
 
-part1 =  p*d
+
+part1 = p*d
 
 print(f'part1: {part1}')
 
-
-p, d, aim = 0, 0, 0
 ops = {
     'forward': lambda v, p, d, a: (p+v, d+(a*v), a),
     'up': lambda v, p, d, a: (p, d, a-v),
     'down': lambda v, p, d, a: (p, d, a+v),
     }
 
-for line in data:
-    op, val = line.split()
-    val = int(val)
-    #print(f'{op} > {val} >', ops[op](val, p, d, aim))
-    p, d, aim = ops[op](val, p, d, aim)
+p, d, aim = 0, 0, 0
+for op, val in data: p, d, aim = ops[op](int(val), p, d, aim)
 
-part2 =  p*d
+part2 = p*d
 print(f'part2: {part2}')
