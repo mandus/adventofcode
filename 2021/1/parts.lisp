e912544095254d844127f558747d96cb4c2358a2
Åsmund Ødegård <mandus@gmail.com>
2021-12-04 23:25:04 +0100

    day 1, lisp cleanup

--- 2021/1/parts.lisp
+++ 2021/1/parts.lisp
@@ -20,64 +20,34 @@
 ;(defparameter *inp* "test_input.txt")
 (defparameter *inp* "input.txt")
 
-; default "transform" for reader
-(defun noop (x) x)
-
-(defun read-input (fn &optional (trans 'noop))
+(defun read-input (fn &optional (trans #'identity))
   (with-open-file (f fn)
     (loop for line = (read-line f nil)
           while line collect (funcall trans line))))
 
-(defun counter (l &optional (acc 0))
-  (if (= 1 (length l)) 
+(defun counter (l cmp &optional (acc 0))
+  (if (= cmp (length l)) 
       acc
-      (let ((x (first l)) 
-            (nxt (rest l)))
-        (if (< x (first nxt)) 
-            (counter nxt (1+ acc))
-            (counter nxt acc)))))
-
-(defun summer (l idx &optional (acc 0))
-  (when *debug* 
-    (format t "s ~a (acc ~a) ~a~%" idx acc l))
-  (if (zerop idx)
-      acc
-      (summer (rest l) (1- idx) (+ acc (first l)))))
-
-(defun countslices (l len &optional (acc 0))
-  (when *debug* 
-    (format t "cs ~a ~a >= ~a~%" acc (length l) len))
-  (if (<= (length l) len)
-      acc
-      (let* ((nxt (rest l))
-            (x (summer l len))
-            (y (summer nxt len)))
-        (when *debug* 
-          (format t "cmp ~a ~a ~%" x y))
-        (if (< x y)
-            (countslices nxt len (1+ acc))
-            (countslices nxt len acc)))))
+      (let* ((x (first l)) 
+             (y (nth cmp l))
+             (nxt (rest l))
+             (nxtacc (if (< x y) (1+ acc) acc)))
+        (counter nxt cmp nxtacc))))
 
 ;; drivers
 ;;
 (defun part1 (fn)
-  (let* ((items (read-input fn #'parse-integer))
-         ;(checks (loop for d in items collect (check-p1 d)))
-         ;(numtrue (count-if-not #'not checks))
-         )
+  (let* ((items (read-input fn #'parse-integer)))
     (format t "Part 1~%")
-    (format t "~a~%" (counter items))
+    (format t "~a~%" (counter items 1))
 
     (when *debug*
       (format t "~a~%" items)) ))
 
 (defun part2 (fn)
-  (let* ((items (read-input fn #'parse-integer))
-         ;(checks (loop for d in items collect (check-p2 d)))
-         ;(numtrue (count-if-not #'not checks))
-         )
+  (let* ((items (read-input fn #'parse-integer)))
     (format t "Part 2~%")
-    (format t "~a~%" (countslices items 3))
+    (format t "~a~%" (counter items 3))
 
     (when *debug*
       (format t "checks: ~a~%" items))))
