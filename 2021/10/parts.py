7de16ef2ecd04f2ce54fa3d875c6e6e000254c66
Åsmund Ødegård <mandus@gmail.com>
2021-12-10 22:49:21 +0100

    day 10, python

--- 2021/10/parts.py
+++ 2021/10/parts.py
@@ -1,31 +1,63 @@
 #!/usr/bin/env python3
 
 
-fn = 'test_input.txt'
-#  fn = 'input.txt'
+#  fn = 'test_input.txt'
+fn = 'input.txt'
 
 
-def checkline(l):
+def chk_line(li):
     stack = []
     opening = '({[<'
     closing = ')}]>'
-    chk_map = {
-            '(': ')',
-            '[': ']',
-            '{': '}',
-            '<': '>'}
+    close_map = {
+            ')': '(',
+            ']': '[',
+            '}': '{',
+            '>': '<',
+            }
 
-    for c in l:
+    illegal_cost = {
+            ')': 3,
+            ']': 57,
+            '}': 1197,
+            '>': 25137,
+            }
+
+    for c in li:
         if c in opening:
             stack.append(c)
-            print(f'st {stack}')
-        elif c in closing and chk_map[c] == stack[-1]:
+        elif c in closing and close_map[c] == stack[-1]:
             stack = stack[:-1]
         else:
-            return False
-    return True
+            return illegal_cost[c], []
+    return 0, stack
+
+
+def comp_line(stack):
+    close_map = {
+            '(': 1,
+            '[': 2,
+            '{': 3,
+            '<': 4,
+            }
+    total = 0
+    for c in reversed(stack):
+        total *= 5
+        total += close_map[c]
+    return total
 
 
 data = [li.strip() for li in open(fn).readlines()]
-print(data)
-non_corrupt = [line for line in data if checkline(line)]
+
+corrupt_sum = 0
+incompletes = []
+for line in data:
+    corrupt, stack = chk_line(line)
+    if corrupt:
+        corrupt_sum += corrupt
+    else:
+        # incomplete
+        incompletes.append(comp_line(stack))
+
+print('part1: ', corrupt_sum)
+print('part2: ', sorted(incompletes)[len(incompletes)//2])
