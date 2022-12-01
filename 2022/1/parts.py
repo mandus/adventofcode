3270c2fc7a4f5adae125e5121a9a64aaf1d40577
Åsmund Ødegård <mandus@gmail.com>
2022-12-01 06:35:47 +0100

    added a simpler solution

--- 2022/1/parts.py
+++ 2022/1/parts.py
@@ -22,3 +22,16 @@ def solve(n, cnt):
 a_n = agg(nums)
 print('part1: ', solve(a_n, 1))
 print('part2: ', solve(a_n, 3))
+
+
+# "Better" solution
+
+data = open('input.txt').read().split('\n\n')
+
+
+def solver(d, cnt):
+    return sum(sorted([sum([int(n) for n in e.split()]) for e in d])[-cnt:])
+
+
+print('part1: ', solver(data, 1))
+print('part2: ', solver(data, 3))
