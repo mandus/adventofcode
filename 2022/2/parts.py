6f9af1fc221dbec5470b8ad0ba6b9fa5218b2bdb
Åsmund Ødegård <mandus@gmail.com>
2022-12-02 13:07:32 +0100

    cleanup day 2 py

--- 2022/2/parts.py
+++ 2022/2/parts.py
@@ -1,39 +1,27 @@
 #!/usr/bin/env python3
 
-#  fn = 'test_input.txt'
+# fn = 'test_input.txt'
 fn = 'input.txt'
 
-# Score:
-ts = {'X': 1, 'Y': 2, 'Z': 3}
+d = [x.split() for x in open(fn).read().strip().split('\n')]
 
-#  X  Y  Z
-res = {'A X': 3,
-       'A Y': 6,
-       'A Z': 0,
-       'B X': 0,
-       'B Y': 3,
-       'B Z': 6,
-       'C X': 6,
-       'C Y': 0,
-       'C Z': 3}
+# Score matrix
+# rows: elf, cols: me
+# 0 = R, 1 = P, 2 = S
+sc = [[4, 8, 3], [1, 5, 9], [7, 2, 6]]
 
-d = open(fn).read().strip().split('\n')
-
-print('part1 ',
-      sum([res[x] for x in d]) +
-      sum([ts[x.split(' ')[1]] for x in d]))
+# Part 1:
+map_elf = {'A': 0, 'B': 1, 'C': 2}
+map_me = {'X': 0, 'Y': 1, 'Z': 2}
+print('part1 ', sum([sc[map_elf[x]][map_me[y]] for (x, y) in d]))
 
 
-# X = win
-# Y = draw
-# Z = loose
-ts = {'R': 1, 'P': 2, 'S': 3}
-res = {'X': 0, 'Y': 3, 'Z': 6}
+# part 2:
+def map_result(elf, me):
+    m = {'X': {'A': 2, 'B': 0, 'C': 1},
+         'Y': {'A': 0, 'B': 1, 'C': 2},
+         'Z': {'A': 1, 'B': 2, 'C': 0}}
+    return m[me][elf]
 
-m = {}
-m['Z'] = {'A': 'P', 'B': 'S', 'C': 'R'}  # Win
-m['Y'] = {'A': 'R', 'B': 'P', 'C': 'S'}  # Draw
-m['X'] = {'A': 'S', 'B': 'R', 'C': 'P'}  # Loose
-print('part2 ',
-      sum([res[x.split()[1]] for x in d]) +
-      sum([ts[m[x.split()[1]][x.split()[0]]] for x in d]))
+
+print('part2 ', sum([sc[map_elf[x]][map_result(x, y)] for (x, y) in d]))
