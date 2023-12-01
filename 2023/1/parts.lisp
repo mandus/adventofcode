346444c26f351bde8e0947d2e2002015a7854c54
Åsmund Ødegård <mandus@gmail.com>
2023-12-01 11:38:00 +0100

    aoc 2023.01 - lisp

--- 2023/1/parts.lisp
+++ 2023/1/parts.lisp
@@ -3,40 +3,97 @@
 ; run: sbcl --quit --load parts.lisp --eval '(aoc:run)'
 
 (ql:quickload :swank)
-(ql:quickload :split-sequence)
+(ql:quickload :str)
 
 (uiop:define-package
   :aoc
   (:use :cl)
   (:export :run))
 
-(unless swank::*connections*
-  (swank:create-server :port 4015 :dont-close t))
 
 (in-package :aoc)
 
-;(defparameter *debug* nil)
 (defparameter *debug* t)
+(defparameter *debug* nil)
 
 (defparameter *inp* "test_input.txt")
-;(defparameter *inp* "input.txt")
+(defparameter *inp* "input.txt")
 
-(defun read-by-para (fn &optional (split "") (trans #'identity))
-  (mapcar trans (split-sequence:split-sequence split (uiop:read-file-lines fn) :test #'equalp)))
+(when *debug*
+  (unless swank::*connections*
+  (swank:create-server :port 4015 :dont-close t)))
 
-(defun split-lines (data)
-  (when *debug*
-    (format t " - ~a ~%" data))
-  (mapcan (lambda (line) (split-sequence:split-sequence #\Newline line)) data))
+(defun read-input (fn &optional (trans #'identity))
+  (with-open-file (f fn)
+    (loop for line = (read-line f nil)
+          while line collect (funcall trans line))))
 
+(defun get-num (val)
+    (let* ((nums (remove-if-not #'digit-char-p val))
+           (l (length nums))
+           (fi (subseq nums 0 1))
+           (la (subseq nums (1- l) l)))
+      (parse-integer (concatenate 'string fi la))))
+
+(defun check-replace (s k v)
+  "if s starts with k, replace with v"
+  (if (str:starts-with-p k s)
+      (str:replace-first k v s)
+      s))
+
+(defun run-match (s p)
+  (let* ((tst (car p))
+         (np (cdr p))
+         (k (car tst))
+         (v (cdr tst))
+         (ns (check-replace s k v)))
+    (if np
+        (run-match ns np)
+        ns)))
+
+(defun matcher (s)
+  (let ((p '(("one" . "1ne")
+             ("two" . "2wo")
+             ("three" . "3hree")
+             ("four" . "4our")
+             ("five" . "5ive")
+             ("six" . "6ix")
+             ("seven" . "7even")
+             ("eight" . "8ight")
+             ("nine" . "9ine")
+             )))
+    (run-match s p)))
+
+(defun traverse (data &optional (r ""))
+  (let* ((s (matcher data))
+         (strl (length s)))
+    (progn
+      (when *debug*
+        (format t "len: ~a for ~a~%" strl s))
+      (if (> strl 0)
+          (traverse (subseq s 1) (concatenate 'string r (subseq s 0 1)))
+          (progn
+            (when *debug*
+              (format t "~% --> will return: ~a~%" r))
+            r)))))
+
+(defun get-num-p2 (val)
+  (let* ((nums (remove-if-not #'digit-char-p (traverse val)))
+         (l (length nums))
+         (fi (subseq nums 0 1))
+         (la (subseq nums (1- l) l)))
+    (progn
+      (when *debug*
+        (format t "~a: ~a (l ~a) [~a : ~a]~%" val nums l fi la))
+      (parse-integer (concatenate 'string fi la)))))
 
 (defun solve (data)
-  (data))
+  (reduce #'+ data))
 
 ;; drivers
 ;;
 (defun part1 (fn)
-  (let* ((items (read-by-para fn "" #'split-lines)))
+  (let* ((items (read-input fn #'get-num)))
     (format t "Part 1~%")
     (format t "~a~%" (solve items))
 
@@ -44,10 +101,9 @@
       (format t "~a~%" items)) ))
 
 (defun part2 (fn)
-  (let* ((items (read-by-para fn "" #'split-lines))
-         )
+  (let* ((items (read-input fn #'get-num-p2)))
     (format t "Part 2~%")
-    (format t "~a~%" "todo")
+    (format t "~a~%" (solve items))
 
     (when *debug*
       (format t "~a~%" items))))
