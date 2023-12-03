f327031b32b013feb7aedc6220ed70f0ff0cd716
Åsmund Ødegård <mandus@gmail.com>
2023-12-03 18:40:07 +0100

    :sparkles: aoc 2023.02 - common lisp

--- 2023/2/parts.lisp
+++ 2023/2/parts.lisp
@@ -4,6 +4,7 @@
 
 (ql:quickload :swank)
 (ql:quickload :str)
+(ql:quickload :cl-ppcre)
 
 (uiop:define-package
   :aoc
@@ -11,13 +12,15 @@
   (:export :run))
 
 
-(in-package :aoc)
+; problem when interning symbols (e.g. red vs. aoc::red) -
+; need to use other keys (string?) in asocciation list
+;(in-package :aoc)
 
-(defparameter *debug* t)
-;(defparameter *debug* nil)
+; (defparameter *debug* t)
+(defparameter *debug* nil)
 
-(defparameter *inp* "t1.txt")
-;(defparameter *inp* "input.txt")
+; (defparameter *inp* "t1.txt")
+(defparameter *inp* "input.txt")
 
 (when *debug*
   (unless swank::*connections*
@@ -28,27 +31,71 @@
     (loop for line = (read-line f nil)
           while line collect (funcall trans line))))
 
+(defun parse (l)
+  (cdr (cl-ppcre:split "[,:;]" l)))
 
-(defun solve (data)
-  data)
+(defun check (g)
+  (let ((goal '(12 13 14))
+        (m (loop for k in '(red green blue)
+                 collect (cdr (assoc k g)))))
+    (every #'identity (mapcar #'>= goal m))))
+
+(defun game (l &optional (al (pairlis '(red green blue) '(0 0 0))))
+  (let* ((f (cl-ppcre:split " " (str:trim (car l))))
+         (c (read-from-string (car (cdr f))))
+         (v (parse-integer (car f)))
+         (cur_v (cdr (assoc c al)))
+         (r (cdr l)))
+    (progn
+      (when *debug*
+        (format t "~a -> ~a|~a [~a]~%" c cur_v v al))
+      (setf (cdr (assoc c al)) (max v cur_v))
+      (if r
+          (game r al)
+          (values (check al) al)))))
+
+(defun power (g)
+  (let ((pwr (reduce #'* (loop for k in '(red green blue)
+                               collect (cdr (assoc k g))))))
+    (progn
+      (when *debug*
+        (format t "power of ~a: ~a~%" g pwr))
+      pwr)))
+
+(defun solve (data &optional (c 1) (e nil) (pwr nil))
+  (let* ((f (car data))
+         (r (cdr data)))
+    (multiple-value-bind (upd al) (game f)
+      (let ((c_inc (if upd c 0))
+            (g_pwr (power al)))
+        (progn
+          (when *debug*
+            (format t "upd: ~a: ~a~%" upd al))
+          (if r
+              (solve r (1+ c) (cons c_inc e) (cons g_pwr pwr))
+              (values (reduce #'+ (cons c_inc e)) (reduce #'+ (cons g_pwr pwr)))))))))
 
 ;; drivers
 ;;
 (defun part1 (fn)
-  (let* ((items (read-input fn)))
+  (let* ((items (read-input fn #'parse)))
     (format t "Part 1~%")
-    (format t "~a~%" (solve items))
+    (multiple-value-bind (g pwr) (solve items)
+      (declare (ignore pwr))
+      (format t "~a~%" g)
 
-    (when *debug*
-      (format t "~a~%" items)) ))
+      (when *debug*
+        (format t "~a~%" items)))))
 
 (defun part2 (fn)
-  (let* ((items (read-input fn)))
+  (let* ((items (read-input fn #'parse)))
     (format t "Part 2~%")
-    (format t "~a~%" (solve items))
+    (multiple-value-bind (g pwr) (solve items)
+      (declare (ignore g))
+      (format t "~a~%" pwr)
 
-    (when *debug*
-      (format t "~a~%" items))))
+      (when *debug*
+        (format t "~a~%" pwr)))))
 
 (defun run ()
   (part1 *inp*)
