ee1b128dbe9c3c7ce8c52731083067bb3d4469fa
Åsmund Ødegård <asmund.odegard@xal.no>
2023-12-08 11:31:58 +0100

    :fire: aoc 2023.08 - python - p2

--- 2023/8/08.py
+++ 2023/8/08.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 
-fn = 't1.txt'
-fn = 't2.txt'
+import math
+
+fn = 't3.txt'
 fn = 'input.txt'
 
 rl, d = open(fn).read().strip().split('\n\n')
 m = {e[0]: {'L': e[2][1:-1], 'R': e[3][:-1]} for e in [x.split() for x in d.split('\n')]}
 
-e = 'ZZZ'
+
+def steps(pos, mvs, m, p2=False):
+    cnt = 0
+    endchk = lambda x: x != 'ZZZ'
+    if p2:
+        endchk = lambda x: x[-1] != 'Z'
+    while endchk(pos):
+        cnt += 1
+        instr = mvs[0]
+        pos = m[pos][instr]
+        mvs = mvs[1:] + instr
+    return cnt
+
 
 pos = 'AAA'
-cnt = 0
-while pos != e:
-    cnt += 1
-    instr = rl[0]
-    pos = m[pos][instr]
-    rl = rl[1:] + instr
+e = 'ZZZ'
+if pos in m:
+    print(f'part1: {steps(pos, rl, m)}')
 
-print(f'part1: {cnt}')
+cs = {p: 0 for p, _ in m.items() if p[-1] == 'A'}
+
+for pos in cs.keys():
+    cs[pos] = steps(pos, str(rl), m, True)
+print(f'part2: {math.lcm(*list(cs.values()))}')
