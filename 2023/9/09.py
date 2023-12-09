2c68b9853ba6db3f6606dec62d79824ca2c227b0
Åsmund Ødegård <asmund.odegard@xal.no>
2023-12-09 08:49:53 +0100

    :sparkles: aoc 2023.09 - python :wip:

---
+++ 2023/9/09.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+fn = 't1.txt'
+fn = 'input.txt'
+
+d = [[int(y) for y in x.split()] for x in open(fn).read().strip().split('\n')]
+
+
+def diff(ln):
+    return [y-x for x, y in zip(ln[:-1], ln[1:])]
+
+
+def scan(ln):
+    lns = [ln]
+    while any(lns[-1]):
+        lns.append(diff(lns[-1]))
+    return sum([ln[-1] for ln in lns])
+
+
+print(f'part1: {sum([scan(x) for x in d])}')
