c1f8879fbba04bdcae2629f89fc292bebb32b919
Åsmund Ødegård <mandus@gmail.com>
2023-12-11 22:29:13 +0100

    :fire: aoc 2023.11 - python - cleanup

--- 2023/11/11.py
+++ 2023/11/11.py
@@ -26,23 +26,17 @@ def transp(d: list) -> list:
 
 def calcdists(x: list, exr: list, exc: list, mul: int = 2) -> list:
     g = [(i, j) for i, ln in enumerate(x) for (j, c) in enumerate(ln) if c == '#']
-    dists = []
-    extra = []
+    dists = 0
     for i, v in enumerate(g):
         xv, yv = v
         for w in g[i+1:]:
             xw, yw = w
-
             xl, xh = sorted([xv, xw])
             yl, yh = sorted([yv, yw])
-
             addx = len([i for i in exc if xl < i < xh])
             addy = len([i for i in exr if yl < i < yh])
-
-            d = abs(xv-xw) + abs(yv-yw)
-            extra.append(addx + addy)
-            dists.append(d)
-    dists.append(sum(extra)*(mul-1))
+            d = abs(xv-xw) + abs(yv-yw) + (addx + addy)*(mul-1)
+            dists += d
     return dists
 
 
@@ -50,5 +44,5 @@ def calcdists(x: list, exr: list, exc: list, mul: int = 2) -> list:
 xcols = extras(transp(x))
 xrows = extras(x)
 
-print(f'part1: {sum(calcdists(x, xcols, xrows))}')
-print(f'part2: {sum(calcdists(x, xcols, xrows, mul=1000000))}')
+print(f'part1: {calcdists(x, xcols, xrows)}')
+print(f'part2: {calcdists(x, xcols, xrows, mul=1000000)}')
