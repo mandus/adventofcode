68c3cc5fdd3d39a7848fe015377209abbdfd63be
Åsmund Ødegård <asmund.odegard@xal.no>
2023-12-12 21:21:43 +0100

    :truck: 2023.12 - wip recurse

--- 2023/12/12.py
+++ 2023/12/12.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-import itertools as it
 
-# fn = 't1.txt'
-fn = 'input.txt'
+fn = 't1.txt'
+# fn = 'input.txt'
 
 
 def upd_pat(pat, qs, upd):
@@ -39,28 +38,67 @@ def chk_pat(pat, chk):
 def c_h(ln):
     return sum(1 for x in ln if x == '#')
 
-def arrs(ln):
+
+def solve(pat, chk, cur=0):
+    #print(f'solve {pat} for {chk} with {cur}')
+    cnt = 0
+    f = pat[0]
+    r = pat[1:]
+
+    if cur and (not chk or chk[0] < cur):
+        return 0
+
+    if not r:
+        # exhausted;
+        if f == '.' and cur and len(chk) == 1 and cur == chk[0]:
+            print(f' | return 1 for {f} with {cur} and {chk}')
+            return 1
+        elif f == '.' and not chk and not cur:
+            print(f' - return 1 for {f} with {cur} and {chk}')
+            return 1
+        elif f == '#' and len(chk) == 1 and chk[0] == cur+1:
+            print(f' > return 1 for {f}, {cur}, {chk[0]}')
+            return 1
+        elif f == '?' and ((len(chk) == 1 and chk[0] == cur+1) or (not chk and not cur)):
+            print(f' < return 1 for {f} with {cur} and {chk}')
+            return 1
+        return 0
+
+    if f == '.' and chk and chk[0] == cur:
+        cnt += solve(r, chk[1:], 0)
+    elif f == '.' and not cur:
+        cnt += solve(r, chk, 0)
+    elif f == '#':
+        cnt += solve(r, chk, cur+1)
+    elif f == '?' and chk and chk[0] == cur:
+        # pretend dot and end group:
+        cnt += solve(r, chk[1:], 0)
+    elif f == '?':
+        # pretend hash:
+        print(f' +++ from {cnt} {pat} - {chk} - {cur}')
+        cnt += solve(r, chk, cur+1)
+        print(f' via {cnt} {pat} and {chk}')
+        # or pretend dot
+        if cur and chk[0] == cur:
+            cnt += solve(r, chk[1:], 0)
+        elif not cur:
+            cnt += solve(r, chk, 0)
+        print(f' got to {cnt} from {pat} and {chk}')
+    else:
+        print(f'  ///// unhandled {pat} {chk} {cur}')
+
+    print(f' >>> return {cnt} for {pat} and {chk} and {cur}')
+    return cnt
+
+
+def cnts(ln):
     pat, chk = ln.split()
     pat = [c for c in pat]
     chk = [int(x) for x in chk.split(',')]
-    t_h = sum(chk)
-    h = c_h(pat)
-    # need to have this amount of '#' in the updates
-    f_h = t_h - h
-
-    # need to find all the permutations of # for the question-marks in pat
-    # that match chk and the rule (at least on . between groups)
-    qs = [i for i, q in enumerate(pat) if q == '?']
-    upds = ("".join(seq) for seq in it.product(".#", repeat=len(qs)) if c_h(seq) == f_h)
-    return sum(1 for u in upds if chk_pat(upd_pat(pat, qs, u), chk))
+    return solve(pat, chk)
 
 
 d = open(fn).read().strip().split('\n')
 
-print(f'part1: {sum([arrs(ln) for ln in d])}')
+print(f'part1: {sum([cnts(ln) for ln in d])}')
 print(f'part2: {2}')
-
-
-# ..#.??.#; 1,1,1
-# ..#.#..#
-# ..#..#.#
