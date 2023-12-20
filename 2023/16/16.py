135267cb97005a9154fc45d22793ea2e30b29baf
Åsmund Ødegård <asmund.odegard@xal.no>
2023-12-20 17:19:23 +0100

    :fire: aoc 2023.16 - python - p1

--- 2023/16/16.py
+++ 2023/16/16.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 fn = 't1.txt'
-# fn = 'input.txt'
+fn = 'input.txt'
+
 
 def add(p, v):
     return (p[0] + v[0], p[1] + v[1])
@@ -27,48 +28,46 @@ def backw(v):
             (-1, 0): (0, -1)}[v]
 
 
-def trace(d: list, p: tuple, v: tuple, m: dict, seen: dict) -> dict:
-    """d: data; p: position; v: vec for direction; m: reached cells
-       return reached cells
-
-       postions/directions: (row, col).
-    """
-    # TODO: consider if same p/v should trigger immediate return with known result?
-
-    # If I get to same place with same direction as before; exit - no need to continue
-    if (p, v) in seen:
-        return m
-    seen[(p, v)] = True
-
+def process(d: list) -> dict:
+    m = {}
+    seen = {}
     sh = (len(d), len(d[0]))
-    nx = add(p, v)
-    if out(sh, nx):
-        # left grid
-        return m
-    m[nx] = m.get(nx, 0) + 1
-    r, c = nx
-    sy = d[r][c]
-    if sy == '.' or (sy == '-' and v[0] == 0) or (sy == '|' and v[1] == 0):
-        return trace(d, nx, v, m, seen)
-    if sy == '-' and v[1] == 0:
-        trace(d, nx, (0, 1), m, seen)
-        return trace(d, nx, (0, -1), m, seen)
-    if sy == '|' and v[0] == 0:
-        trace(d, nx, (1, 0), m, seen)
-        return trace(d, nx, (-1, 0), m, seen)
-    if sy == '/':
-        return trace(d, nx, forw(v), m, seen)
-    if sy == '\\':
-        return trace(d, nx, backw(v), m, seen)
-    raise Exception(f'should not get here with {nx} and {v} at {sy}')
+
+    nxt = [((0, -1), (0, 1))]
+    while nxt:
+        p, v = nxt.pop()
+
+        if (p, v) in seen:
+            # been here before
+            continue
+        seen[(p, v)] = True
+
+        # shine the light:
+        nx = add(p, v)
+        if out(sh, nx):
+            # left grid
+            continue
+        m[nx] = m.get(nx, 0) + 1
+        r, c = nx
+        sy = d[r][c]
+        if sy == '.' or (sy == '-' and v[0] == 0) or (sy == '|' and v[1] == 0):
+            nxt.append((nx, v))
+            continue
+        if sy == '-' and v[1] == 0:
+            nxt.extend([(nx, (0, 1)), (nx, (0, -1))])
+            continue
+        if sy == '|' and v[0] == 0:
+            nxt.extend([(nx, (1, 0)), (nx, (-1, 0))])
+            continue
+        if sy == '/':
+            nxt.append((nx, forw(v)))
+            continue
+        if sy == '\\':
+            nxt.append((nx, backw(v)))
+    return m
 
 
 d = [[c for c in x] for x in open(fn).read().strip().split('\n')]
-m = trace(d, (0, -1), (0, 1), {}, {})
-
-# for r in range(len(d)):
-#     for c in range(len(d[0])):
-#         print('#' if (r, c) in m else '.', end='')
-#     print()
+m = process(d)
 
 print(f'part1: {len(m)}')
